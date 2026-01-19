1ad60f517bc157c462d5f250d1506a3b08d592a0
Phongha <phongha@gmail.com>
2026-01-19 17:48:29 +0700

    Add btn-menu action

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,15 @@ async function addComponent(id, patch) {
   const a = await (await fetch(patch)).text();
   document.getElementById(id).innerHTML = a;
 }
-
 addComponent("header", "./src/components/header.html");
 addComponent("sidebar", "./src/components/sidebar.html");
+
+let sidebar = document.getElementById("sidebar");
+function closeSidebar() {
+  sidebar.style.display = "none";
+  document.getElementById("menu-btn").style.display = "block";
+}
+function openSidebar() {
+  sidebar.style.display = "block";
+  document.getElementById("menu-btn").style.display = "none";
+}
